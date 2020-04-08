f1409b68fe41715274ea10ecaac047f1318dd57c
Lelu <Lelu@DESKTOP-T62NTH6>
2020-04-08 12:23:49 +0700

    Menambah variabel firstname, lastname dan alamat

--- src/Model/adminModel.java
+++ src/Model/adminModel.java
@@ -13,6 +13,9 @@ public class adminModel {
     private int id;
     private String username;
     private String password;
+    private String firstName;
+    private String lastName;
+    private String alamatAdmin;
 
     /**
      * @return the id
@@ -54,6 +57,48 @@ public class adminModel {
      */
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the alamatAdmin
+     */
+    public String getAlamatAdmin() {
+        return alamatAdmin;
+    }
+
+    /**
+     * @param alamatAdmin the alamatAdmin to set
+     */
+    public void setAlamatAdmin(String alamatAdmin) {
+        this.alamatAdmin = alamatAdmin;
     }
     
     
